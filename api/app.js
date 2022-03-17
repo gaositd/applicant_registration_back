3475fd168ccc3f85c4ccbeed9e0368f32330ede0
Alvaro Gamez Chavez <gaositd@gmail.com>
2022-03-17 12:51:14 -0600

    back end structure examples

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 //create web server with nodeJs and express configuration
-const express = require('express');
-const port = 3001;
-const server = express();
+// const express = require('express');
+// const port = 3001;
+// const server = express();
 
 // server.get('/', function(req, res) {
 //     res.send('Hola Mundo!');
@@ -10,3 +10,17 @@ const server = express();
 //   server.listen(port, function() {
 //     console.log(`Aplicación ejemplo, escuchando el puerto ${port}`);
 //   });
+
+// Use Ecmascript 6 functionality
+'use strict'
+var express = require('express');
+var bodyParser = require('body-parser');// Load express and body-parser modules
+var server = express();// We call express in order to create the server
+var user_routes = require('./routes/user');// Import routes
+ //Load middlewares
+ //un metodo que se ejecuta antes que llegue a un controlador
+
+// app.use(bodyParser.urlencoded({extended:false}));//Configuramos bodyParser para que convierta el body de nuestras peticiones a JSON
+// app.use(bodyParser.json());
+// app.use('/api', user_routes);// Cargamos las rutas
+// module.exports = app;// exportamos este módulo para poder usar la variable app fuera de este archivo
--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
 //database connection and general configuration
+// Use Ecmascript 6 functionality
+'use strict'
+var sequelize = require('sequelize');// Cargamos el módulo de mongoose para poder conectarnos a MongoDB
+var app = require('./app');// *Load the app.js file with Express configuration
+var port = 3001;//Create the PORT variable to indicate the port on which the server is going to operate
+// mongoose.Promise = global.Promise;// To indicate to Sequelize that we will make the connection with Promises
+// mongoose.connect('mongodb://localhost:27017/curso_mean_social',  { useMongoClient: true})// use the connect method to connect to our database
+//     .then(() => {        
+//         console.log("The connection to the database was successful.")// CWhen the connection is made, we launch this message by console
+    
+//         app.listen(port, () => {// CREATE THE WEB SERVER WITH NODEJS
+//             console.log(`server run on http://localhost:${port}`);
+//         });
+//     })
+//     .catch(err => console.log(err));//If it does not connect correctly we spit out the error
