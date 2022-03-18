77db4004d581061a2a7b38bf3112ebd5f0e46ad7
Alvaro Gamez Chavez <gaositd@gmail.com>
2022-03-18 11:38:13 -0600

    try to download and config mysql

---
+++ api/routes/form/routes.js
@@ -0,0 +1,8 @@
+//app routes
+//user example
+// 'use strict'// Cargamos el módulo de express para poder crear rutas
+// var express = require('express');// Cargamos el controlador
+// var UserController = require('../controllers/user');// Llamamos al router
+// var api = express.Router();var md_auth = require('../middlewares/authenticated');// Creamos una ruta para los métodos que tenemos en nuestros controladores
+// api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);// Exportamos la configuración
+// module.exports = api;
