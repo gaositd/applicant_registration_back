14f9e73d6f83a015e18085619f78d3717b5cae10
Jonathan Marquez <zomodan2306@gmail.com>
2022-11-26 15:39:52 -0600

    feat/create users endpoints

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from .routers import users
 
+
 app = FastAPI()
 
 
 @app.get("/")
 def helloWorld():
-    return "Hello World!"
+    return {"data": "alo"}
 
 app.include_router(router=users.router, prefix="/users", tags=['users'])
--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,31 @@
-from pydantic import BaseModel
+from sqlalchemy import Table,Column
+from sqlalchemy.sql.sqltypes import Integer,String, Date, Enum, Boolean
+from ..config.db import meta,engine
+import enum
+class genders(enum.Enum):
+    hombre= "H"
+    mujer = "M"
+    otro = "?"
 
+users = Table("users", meta, 
+    Column("id", Integer, primary_key=True),
+    Column("name", String(255)),
+    Column("lastName", String(255)),
+    Column("mail", String(255)),
+    Column("birthDate",Date),
+    Column("gender",Enum(genders)),
+    Column("school",String(255)),
+    Column("phone",String(255)),
+    Column("isWorking", Boolean),
+    Column("hasDisability",Boolean),
+    Column("street", String(255)),
+    Column("extNum", String(255)),
+    Column("col", String(255)),
+    Column("zipCode", String(255)),
+    Column("knowsDialect", Boolean),
+    Column("state", String(255)),
+    Column("city",String(255)),
+    Column("curp", String(255))
+)
 
-class User(BaseModel):
-    id:int
-    name:str
-    lastName:str
-    mail:str
+meta.create_all(engine)
