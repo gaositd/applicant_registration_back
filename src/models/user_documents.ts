be184b11ed77c983123a2a577cde2a919e01d8c2
Jonathan Marquez <zomodan2306@gmail.com>
2023-01-25 14:18:09 -0600

    feat(models): created config table to manage the values and configs

--- src/models/user_documents.ts
+++ src/models/user_documents.ts
@@ -1,18 +1,15 @@
-import { Enum, ManyToOne, Property } from '@mikro-orm/core';
+import {
+  Collection,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  Property,
+} from '@mikro-orm/core';
 import { BaseModel } from './base';
+import { Configuration } from './configs';
 import { User } from './user';
 
-enum TIPO_DOCUMENTO {
-  CURP = 'curp',
-  ACTA_NACIMIENTO = 'acta_nacimiento',
-}
-
-export type TIPO_DOCUMENTO_TYPE = Record<TIPO_DOCUMENTO, string>;
-
 export class UserDocuments extends BaseModel {
-  @Enum(() => TIPO_DOCUMENTO)
-  tipo_documento: TIPO_DOCUMENTO_TYPE;
-
   @Property()
   ruta: string;
 
@@ -24,4 +21,7 @@ export class UserDocuments extends BaseModel {
 
   @ManyToOne(() => User)
   user: User;
+
+  @ManyToMany({ entity: () => Configuration, owner: true })
+  documentos = new Collection<Configuration>(this);
 }
