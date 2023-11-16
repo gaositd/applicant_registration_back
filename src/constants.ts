b4729eb4388ac5a68177f97c250db5a8d07a2130
Jonathan Marquez <zomodan2306@gmail.com>
2023-11-16 13:59:39 -0600

    fix(constants): updated constants on messages of activityHistory

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const DOCUMENTS_OPERATIONS_MESSAGES = {
+  approve: 'El documento ha sido aprobado',
+  reject: 'El documento ha sido rechazado',
+  reviewing: 'El documento ha sido enviado a revisión',
+  'open-to-upload': 'El documento ha sido abierto para subir',
+  upload: 'El documento ha sido subido',
+};
+
+export const USER_OPERATIONS_MESSAGES = {
+  'create-admin': 'Se ha creado un nuevo administrador o secretaria',
+  create: 'Se ha creado un nuevo prospecto',
+  update: 'Se ha actualizado un usuario',
+  delete: 'Se ha eliminado un usuario',
+};
