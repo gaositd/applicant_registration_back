4bb5ec9b5191a63aaad6b751e7daaae87b419d5e
Alvaro Gamez Chavez <gaositd@gmail.com>
2024-02-03 12:20:41 -0600

    notification (controller & services) tests, not checked

--- src/notifications/notifications.controller.spec.ts
+++ src/notifications/notifications.controller.spec.ts
@@ -22,4 +22,33 @@ describe('NotificationsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('getNotifications', () => {
+    it('should return notification',() =>{
+      const notification = controller.getNotifications();
+      expect(notification).toEqual({});
+    });
+  });
+
+  describe('createNotification', () =>{
+    it('should create notification',() =>{
+      const notification = controller.createNotification();
+      expect(notification).toEqual({});
+    });
+  });
+
+  describe('createNotificationToUser', () =>{
+    it('should create notification to user',() =>{
+      const notification = controller.createNotificationToUser();
+      expect(notification).toEqual({});
+    });
+  });
+
+  describe('updateNotification', () =>{
+    it('should update notification',() =>{
+      const notification = controller.updateNotification();
+      expect(notification).toEqual({});
+    });
+  });
+  
 });
--- src/notifications/notifications.service.spec.ts
+++ src/notifications/notifications.service.spec.ts
@@ -1,7 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { NotificationsService } from './notifications.service';
 import { EntityManager } from '@mikro-orm/postgresql';
+import { Test, TestingModule } from '@nestjs/testing';
 import { ActivityHistoryService } from '../activity-history/activity-history.service';
+import { NotificationsService } from './notifications.service';
 
 describe('NotificationsService', () => {
   let service: NotificationsService;
@@ -26,5 +26,33 @@ describe('NotificationsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createNotification', () => {
+    it('should create Notification', async() => {
+      const notification = await service.createNotification();
+      expect(notification).toEqual('');;
+    });
+  });
+
+  describe('getNotifications', () => {
+    it('should get notifications', async() => {
+      const notification = await service.getNotifications();
+      expect(notification).toEqual({});
+    });
+  });
+
+  describe('isUserExpedienteEnabled', () => {
+    it('should return true', async() => {
+      const notification = await service.isUserExpedienteEnabled();
+      expect(notification).toEqual(true);
+    });
+  });
+
+  describe('addressNotification', () =>{
+    it('should return an object notification', async() => {
+      const notification = await service.addressNotification();
+      expect(notification).toEqual({});
+    });
   });
 });
