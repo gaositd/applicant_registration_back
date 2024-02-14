87d2c4e1820f4a044055ea40484045e8ce61da29
Alvaro Gamez Chavez <gaositd@gmail.com>
2024-02-13 19:22:08 -0600

    user test finished

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,8 +1,12 @@
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import supertest, { Response } from 'supertest';
 import { AuthController } from './auth.controller';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let request: any;
+  let app: INestApplication;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,9 +14,41 @@ describe('AuthController', () => {
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+
+    app = module.createNestApplication(); // Crea la aplicación a partir del módulo de test
+    await app.init();
+    request = supertest(app.getHttpServer());
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('me', () => {
+    it('should be me information', async() => {
+      const me: Response = await request.get('/auth/me');
+
+      expect(me.status).toBe(200);
+      expect(me.body).toEqual({});
+    });
+  });
+
+  describe('login', () => {
+    it('should be me login', async() => {
+      const login: Response = await request.post('/auth/login');
+
+      expect(login.status).toBe(200);
+      expect(login.body).toEqual({});
+    });
+  });
+
+  describe('logout', () => {
+    it('should be nothing', async() => {
+      const logout: Response = await request.delete('/auth/logout');
+
+      expect(logout.status).toBe(200);
+      expect(logout.body).toEqual('');
+    });
+  });
+
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,19 +1,50 @@
+import { EntityManager } from '@mikro-orm/postgresql';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { UserMock } from 'src/users/testing/userService.mocks';
 import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let em:EntityManager;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService, { provide: UsersService, useValue: {} }],
+      providers: [AuthService, { provide: UsersService, useValue: {
+        fork: jest.fn(() => ({
+          findOne: jest.fn().mockReturnValue(UserMock),
+        })),
+        find: jest.fn(),
+      } }],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    em = module.get<EntityManager>(AuthService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return null if user is not found', async () => {
+    const user = await service.validateUser('matricula', 'password');
+    expect(user).toBeNull();
+  });
+
+  describe('validateUser', () => {// Simula comparación fallida
+    it('should return null if user is not found', async () => {
+      jest.spyOn(em, 'find').mockImplementation(async () => []);
+
+      const user = await service.validateUser('12345','wrongPassword');
+      expect(user).toBeNull();
+    });
+    
+    it('should return null if user is not found', async () => {
+      jest.spyOn(em, 'find').mockImplementation(async () => []);
+  
+      const user = await service.validateUser('1234567890','1234567890');
+      expect(user).toEqual({UserMock});
+    });
+  });
+
 });
