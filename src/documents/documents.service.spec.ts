f10a6a6e79985ec36b37e8fab3f0511897e8b79c
Alvaro Gamez Chavez <gaositd@gmail.com>
2024-02-14 19:14:47 -0600

    mail test finished

--- src/documents/documents.service.spec.ts
+++ src/documents/documents.service.spec.ts
@@ -7,7 +7,7 @@ import { DocumentsService } from './documents.service';
 
 describe('DocumentsService', () => {
   let service: DocumentsService;
-
+  let em:EntityManager;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -32,6 +32,8 @@ describe('DocumentsService', () => {
     }).compile();
 
     service = module.get<DocumentsService>(DocumentsService);
+    em = module.get<EntityManager>(EntityManager);
+
   });
 
   it('should be defined', () => {
@@ -39,38 +41,38 @@ describe('DocumentsService', () => {
   });
 
   describe('findDocs', () =>{
-    it('Should return an document object', () =>{
-      jest.spyOn(em, 'find').mockImplementation(async () => {});
-      const doc = await service.findDocs();
+    it('Should return an document object', async () =>{
+      jest.spyOn(em, 'find').mockImplementation( );
+      const doc = await service.findDocs;
       expect(doc).toEqual({});
     });
   });
 
   describe('findDocsById', () => {
-    it('Should return an document object', () =>{
-      jest.spyOn(em, 'find').mockImplementation(async () => {});
-      const doc = await service.findDocsById();
+    it('Should return an document object', async() =>{
+      jest.spyOn(em, 'find').mockImplementation( );
+      const doc = await service.findDocsById;
       expect(doc).toEqual({});
     });
   });
 
   describe('updateDocumentStatus', () =>{
-    it('Should return an update document status', () =>{
-      const doc = await service.updateDocumentStatus();
+    it('Should return an update document status', async () =>{
+      const doc = await service.updateDocumentStatus;
       expect(doc).toEqual(true);
     });
   });
 
   describe('uploadDocument', () =>{
-    it('Should return an object Upload File', () =>{
-      const doc = await service.uploadDocument();
+    it('Should return an object Upload File', async () =>{
+      const doc = await service.uploadDocument;
       expect(doc).toEqual({});
     });
   });
 
   describe('findDocsFile', () => {
-    it('Should return a object Documet File', () =>{
-      const doc = await service.findDocsFile();
+    it('Should return a object Documet File', async() =>{
+      const doc = await service.findDocsFile;
       expect(doc).toEqual({});
     });
   });
--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -2,9 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MailService } from './mail.service';
 import { MailerService } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
+import { mailMock } from 'src/users/testing/mailService.mock';
 
 describe('MailService', () => {
   let service: MailService;
+  // const {to, from, subject, template, context} = mailMock;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,4 +29,12 @@ describe('MailService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("sendEmail", () =>{
+    it('Shouls return a email',async () => {
+      const email = await service.sendMail(mailMock);
+      expect(email).toEqual(null);
+    })
+  });
+
 });
