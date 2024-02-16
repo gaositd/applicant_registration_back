3fafb251ae4e808108c43d56d3df6c133981abc5
Alvaro Gamez Chavez <gaositd@gmail.com>
2024-02-15 19:02:25 -0600

    mail test finished

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,12 +1,12 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { MailService } from './mail.service';
 import { MailerService } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
 import { mailMock } from 'src/users/testing/mailService.mock';
+import { MailService } from './mail.service';
 
 describe('MailService', () => {
   let service: MailService;
-  // const {to, from, subject, template, context} = mailMock;
+  const {to, from, subject, template, context} = mailMock;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -32,7 +32,7 @@ describe('MailService', () => {
 
   describe("sendEmail", () =>{
     it('Shouls return a email',async () => {
-      const email = await service.sendMail(mailMock);
+      const email = await service.sendMail(to,subject,template,context);
       expect(email).toEqual(null);
     })
   });
--- src/users/testing/mailService.mock.ts
+++ src/users/testing/mailService.mock.ts
@@ -1,7 +1,7 @@
 export const mailMock = {
- "to" : "test@test.com",
- "from":"test1@test1.com",
- "subject": "loremipsum text",
- "template":"Magna cillum duis aliquip veniam officia eu Lorem aliquip velit eiusmod. Culpa mollit consequat in excepteur laborum laboris eu incididunt ad. Pariatur eu consectetur id consequat cillum fugiat qui in ex dolore. Id voluptate consectetur officia duis veniam magna culpa nostrud reprehenderit. Nulla enim ea ea dolore eu do. Reprehenderit sint exercitation sint nisi sit. Aliquip irure minim culpa enim adipisicing eu amet amet voluptate.",
- "context":"Magna cillum duis aliquip veniam officia eu Lorem aliquip velit eiusmod. Culpa mollit consequat in excepteur laborum laboris eu incididunt ad. Pariatur eu consectetur id consequat cillum fugiat qui in ex dolore. Id voluptate consectetur officia duis veniam magna culpa nostrud reprehenderit. Nulla enim ea ea dolore eu do. Reprehenderit sint exercitation sint nisi sit. Aliquip irure minim culpa enim adipisicing eu amet amet voluptate."
-}
+  "to" : "test@test.com",
+  "from":"test1@test1.com",
+  "subject": "loremipsum text",
+  "template":"Magna cillum duis aliquip veniam officia eu Lorem aliquip velit eiusmod. Culpa mollit consequat in excepteur laborum laboris eu incididunt ad. Pariatur eu consectetur id consequat cillum fugiat qui in ex dolore. Id voluptate consectetur officia duis veniam magna culpa nostrud reprehenderit. Nulla enim ea ea dolore eu do. Reprehenderit sint exercitation sint nisi sit. Aliquip irure minim culpa enim adipisicing eu amet amet voluptate.",
+  "context":"Magna cillum duis aliquip veniam officia eu Lorem aliquip velit eiusmod. Culpa mollit consequat in excepteur laborum laboris eu incididunt ad. Pariatur eu consectetur id consequat cillum fugiat qui in ex dolore. Id voluptate consectetur officia duis veniam magna culpa nostrud reprehenderit. Nulla enim ea ea dolore eu do. Reprehenderit sint exercitation sint nisi sit. Aliquip irure minim culpa enim adipisicing eu amet amet voluptate.",
+ }
